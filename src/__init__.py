53f1269b3692f78b0ea56a9af486512bcb2f5b6e
BasHV456 <bashoogeventerink@gmail.com>
2025-09-30 17:05:39 +0000

    removed empty py-files

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# src/mymodule/__init__.py
+# from mymodule.main import some_function
+
+# Explicitly declare public API
+__version__ = "0.1.0" # now people can run mymodule.__version__ to check the version
