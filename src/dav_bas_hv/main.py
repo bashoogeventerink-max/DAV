a06641b9aaa05b308f5272d7ef1605b3dc53fb2b
BasHV456 <bashoogeventerink@gmail.com>
2025-10-18 11:33:21 +0000

    update main and delete old parq files

--- src/dav_bas_hv/main.py
+++ src/dav_bas_hv/main.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from test import test_function
 from preprocess import main as preprocess_main 
 
+# Load toml configuration
 def load_config(config_path="config.toml"):
     try:
         with open(config_path, "rb") as f:
@@ -16,17 +17,29 @@ def load_config(config_path="config.toml"):
         return None
 
 def main():
+    
+    # Check if data already exists
     config = load_config()
     if not config:
         return
     
-    processed_filename = config[""]
+    processed_filename = config["inputpath"]
+    data_folder_str = Path("data/processed").resolve()
+
+    #convert data folder string to a path object
+    data_folder = Path(data_folder_str)
+    processed_filepath = data_folder / processed_filename
+
+    if processed_filepath.exists():
+        print(f"Processed file '{processed_filename} already exists at '{processed_filepath}'. Skipping preprocessing.")
+    else:
+        print(f"Processed file '{processed_filename}' not found. Running preprocessing...")
     
-    
-    
+        preprocess_main()
+
+        print("Preprocessing completed.")
     
     test_function()
-    preprocess_main()
 
 if __name__ == '__main__':
     main()
